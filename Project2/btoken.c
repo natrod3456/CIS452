a19ad55fe453b1e87f4958a642731808b8a5e1de
Natalie Rodriguez <natrod3456@gmail.com>
2018-10-03 23:10:22 -0400

    done

--- Project2/btoken.c
+++ Project2/btoken.c
@@ -13,25 +13,33 @@ struct Token{
 };
 
 struct Token token;
+struct Token token2;
 int  numMachines;
 int destination;
 
-void sigHandler(int);
-
+/*************************************************************************************************
+ * A program that uses communicating multiple processes to emulate a form of network communication.
+ * B-Level.
+ * @author: Natalie Rodriguez
+ * @version: Fall 2018
+ *************************************************************************************************/
 int main(int argc, char **argv)
 {	
 	char input[5000];
-	char buffer[5000];
 	
-	signal (SIGINT, sigHandler); 
 	if(argc != 3){
-		printf("Please enter the commands as followd: /\a.out [NUMMACHINES] [DESTINATION] \n");
+		printf("Please enter the commands as followed: /\a.out [NUMMACHINES] [DESTINATION] \n");
 		exit(1);
 	
 	}
 
 	numMachines = strtol(argv[1], NULL, 10);
 	destination = strtol(argv[2], NULL, 10);
+
+	if(destination > numMachines){
+		printf("Please enter valid destination\n");
+		exit(0);
+	}
 
 	pid_t pids[numMachines];
 
@@ -55,18 +63,32 @@ int main(int argc, char **argv)
         	exit(1);
 	}
 	else if(pids[0] == 0){
-	//child 1 process
+	/* child processes */
 		for(int x = 1; x < numMachines; x++){
 			if((pids[x] = fork()) < 0){
 				perror("fork failed");
 				exit(1);
 			}	
 			else if(pids[x] == 0){
-				read(fd[x-1][0], buffer, sizeof(buffer));
+				read(fd[x-1][0], &token2, sizeof(struct Token));
 				printf("Child: %d  received message\n", x);
-			//	printf("message is: %s \n", buffer);
+				if(token2.id == x){
+					printf("This is my message...printing message..\n");
+					printf("Message: %s\n", token2.message);
+					token2.id = 0;
+					strcpy(token2.message, "");
+					printf("Setting id to 0 and message to empty...\n");
+					write(fd[x][1], &token2, sizeof(struct Token));
+					printf("Child %d sending message\n", x);
+				}
+				else{
+					printf("This is not my message\n");
+					write(fd[x][1], &token2, sizeof(struct Token));
+					printf("Child %d sending message\n", x);
+				}
+
 				if(x == (numMachines - 1)){
-					printf("Message is: %s\n", buffer);
+					printf("Finished message is: %s\n", buffer);
 					for(int y = 0; y < numMachines; y++){
 						close(fd[y][0]);
 						close(fd[y][1]);
@@ -74,9 +96,6 @@ int main(int argc, char **argv)
 					printf("All pipes closed..goodbye!\n");
 					exit(0);
 				}
-				write(fd[x][1], buffer, (size_t) strlen (buffer) + 1);
-				printf("Child %d sending message\n", x);
-			//	printf("message is: %s \n", buffer);
 			}
 			else{
 				wait(NULL);
@@ -86,16 +105,12 @@ int main(int argc, char **argv)
 
 	}
 	else{
-	//parent 1 process
-		write(fd[0][1], input, (size_t) strlen(input) + 1);
+	/* parent 1 process */
+		token.id = destination;
+		strcpy(token.message, input);
+		write(fd[0][1], &token, sizeof(struct Token));
 		printf("Parent1 sending message\n");
-	//	printf("Message is: %s \n", input);
           	wait(NULL);
 	}
 
-}
-
-void sigHandler(int sigNum)
-{
-	exit(0);
 }
